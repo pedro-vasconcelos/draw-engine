3176ac752c9c72100c49169bec581d19a1af790e
Pedro Vasconcelos <me@pedrovasconcelos.com>
2021-09-04 00:22:50 +0100

    Composer Init

--- src/DrawServiceProvider.php
+++ src/DrawServiceProvider.php
@@ -1,1 +1,25 @@
 <?php
+
+namespace PedroVasconcelos\DrawEngine;
+
+class DrawServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+}
