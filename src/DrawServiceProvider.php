920ccb8bebb9fd8c5950d7aff5d13f1bb945b248
Pedro Vasconcelos <me@pedrovasconcelos.com>
2021-09-19 17:46:39 +0100

    Fixes

--- src/DrawServiceProvider.php
+++ src/DrawServiceProvider.php
@@ -47,6 +47,11 @@ class DrawServiceProvider extends ServiceProvider
                     __DIR__ . '/../database/migrations/create_winners_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_winners_table.php'),
                 ], 'migrations');
             }
+            if (! class_exists('CreateDrawsTable')) {
+                $this->publishes([
+                    __DIR__ . '/../database/migrations/create_draws_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_draws_table.php'),
+                ], 'migrations');
+            }
             // Export Config
             $this->publishes([
                 __DIR__.'/../config/draw-engine.php' => config_path('draw-engine.php'),
